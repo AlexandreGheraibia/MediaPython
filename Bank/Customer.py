fec3e960ed6806cae32ca67dc4216a43de40ea26
AlexandreGheraibia <gheraibia@hotmail.com>
2018-06-30 20:20:21 +0200

    package

---
+++ Bank/Customer.py
@@ -0,0 +1,19 @@
+class Customer:
+    def setId(this,id):
+        return id
+
+    def getId(this):
+        return this.id
+
+    def setName(this,name):
+        return name
+
+    def getName(this,name):
+        return name
+
+    def __init__(this):
+        return
+    
+    def __init__(this,id,name):
+        this.id=id
+        this.name=name
